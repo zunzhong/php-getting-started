f560198a81798b80417944e5eb6bb6112cf22a40
zunzhong <54426972+zunzhong@users.noreply.github.com>
2020-02-24 11:25:04 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -23,3 +23,10 @@ $app->get('/', function() use($app) {
 });
 
 $app->run();
+
+// Cowsay:
+
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
